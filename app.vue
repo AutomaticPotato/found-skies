7d29d228b7823ff3b22fc7efdf71ae434b0abc51
AutomaticPotato <automaticpotato@pm.me>
2025-04-23 09:58:54 +0200

    Add GitHub link with icon to the app footer
    This commit adds a new link to the app's footer, pointing to the project's GitHub repository. The link includes a GitHub icon and is positioned at the bottom-right corner for better visibility.

--- app.vue
+++ app.vue
@@ -4,5 +4,8 @@
     <div class="absolute top-3 right-5">
      Map by NoxX, Cailco, Disort & <a class="underline" href="https://discord.gg/lost-skies-947796968669851659">Community</a>
     </div>
+    <a class="absolute bottom-3 right-5 text-5xl" target="_blank" href="https://github.com/AutomaticPotato/found-skies">
+      <Icon name="mdi:github" />
+    </a>
   </div>
 </template>

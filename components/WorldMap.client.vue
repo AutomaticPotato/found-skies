7e499e2ddebb845a9c4d816c9657245ecf50e953
AutomaticPotato <automaticpotato@pm.me>
2025-04-25 07:54:50 +0200

    Refactor map pin popup creation to a separate utility.
    Extracted the `buildPopup` function into a reusable `createMapPinPopup` utility in a dedicated file.

--- components/WorldMap.client.vue
+++ components/WorldMap.client.vue
@@ -3,6 +3,7 @@ import {islandData} from "~/data/islandData";
 import L from 'leaflet'
 import type {IslandDetails} from "~/types/IslandDetails";
 import 'leaflet/dist/leaflet.css';
+import {createMapPinPopup} from "~/utils/createMapPinPopup";
 
 
 const leafletMap = ref<L.Map>()
@@ -37,7 +38,7 @@ const addMarkersToMap = () => {
       fillColor: '#f03',
       fillOpacity: 0.8,
       radius: 5,
-    }).bindPopup(buildPopup(island))
+    }).bindPopup(createMapPinPopup(island))
         .addTo(leafletMap.value!);
 
 
@@ -48,52 +49,6 @@ const addMarkersToMap = () => {
   });
 };
 
-const buildPopup = (islandDetails: IslandDetails) => {
-  return `
-  <div>
-            <h2 class="text-xl">${islandDetails.name} - ${islandDetails.id}</h2>
-
-            <div class="grid grid-cols-[auto_1fr] items-center gap-3 mt-3  break-words">
-              <img src="icons/altitude.svg" class="size-5" />
-              <span>${islandDetails.altitude || "Unknown"}</span>
-
-              <img src="icons/swords.svg" class="size-5" />
-              <span>${islandDetails.difficulty || "Unknown"}</span>
-
-              <span title="Ark">Ark</span>
-              <span>${islandDetails.ark || "Unknown"}</span>
-
-              <img src="icons/brain.svg" class="size-5" />
-              <span>${islandDetails.databanks || "Unknown"}</span>
-
-              <img src="icons/pickaxe.svg" class="size-5" />
-              <span>${islandDetails.metals?.join(', ') || "Unknown"}</span>
-
-              <img src="icons/tree.svg" class="size-5" />
-              <span>${islandDetails.wood?.join(', ') || "Unknown"}</span>
-
-              <img src="icons/leaf.svg" class="size-5" />
-              <span>${islandDetails.plants?.join(', ') || "Unknown"}</span>
-
-              <img src="icons/package.svg" class="size-5" />
-              <span>${islandDetails.items?.join(', ') || "Unknown"}</span>
-
-              <img src="icons/paw.svg" class="size-5" />
-              <span>${islandDetails.animals?.join(', ') || "Unknown"}</span>
-
-              <img src="icons/treasure.svg" class="size-5" />
-              <span>${islandDetails.chestItems?.join(', ') || "Unknown"}</span>
-
-            <div class="flex flex-col mt-3 ">
-              <span class="font-bold">Note:</span>
-              <p class="max-w-80">
-                ${islandDetails.note || "No notes"}
-              </p>
-            </div>
-            </div>
-  </div>
-  `
-}
 </script>
 
 <template>

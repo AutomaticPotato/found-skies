6703a21e1b35e5d2c22b506b226fb8b88a3a8dd0
AutomaticPotato <automaticpotato@pm.me>
2025-04-25 06:46:12 +0200

    Changed over to leaflet

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="w-screen h-[100dvh]  relative">
-    <WorldMap />
+  <div class="w-screen h-screen overflow-hidden  relative bg-blue-100">
+    <WorldMap/>
     <div class="absolute top-3 right-5">
      Map by NoxX, Cailco, Disort & <a class="underline" href="https://discord.gg/lost-skies-947796968669851659">Community</a>
     </div>
-    <a class="absolute bottom-3 right-5 text-5xl" target="_blank" href="https://github.com/AutomaticPotato/found-skies">
+    <a class="absolute bottom-5 right-5 text-5xl" target="_blank" href="https://github.com/AutomaticPotato/found-skies">
       <Icon name="mdi:github" />
     </a>
   </div>
--- types/IslandDetails.ts
+++ types/IslandDetails.ts
@@ -2,12 +2,17 @@ export type IslandDetails = {
     name: string,
     id: number,
     amountOfKnowledgeNodes?: number
-    xCoordinate?: number
-    yCoordinate?: number
-    xPercentage?: number
-    yPercentage?: number
+    xCoordinate: number
+    yCoordinate: number
     altitude?: number
     difficulty?: number
     ark?: boolean,
-    note?: string
+    note?: string,
+    databanks?: number
+    metals: string[],
+    wood: string[],
+    plants: string[],
+    items: string[],
+    animals: string[],
+    chestItems: string[],
 }
